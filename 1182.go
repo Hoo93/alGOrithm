1d0f4ff4dac6c9bbcff67a6edbbd77d83e94b8f9
Sang Hoo Park <sksk8922@gmail.com>
2024-08-18 00:39:34 +0900

    refactor: 백준1182 부분수열의 합 io 부분 최적화

--- 1182.go
+++ 1182.go
@@ -1,75 +1,72 @@
-//package main
-//
-//import (
-//	"bufio"
-//	"fmt"
-//	"os"
-//	"sort"
-//	"strconv"
-//	"strings"
-//)
-//
-//var (
-//	sc     = bufio.NewScanner(os.Stdin)
-//	wr     = bufio.NewWriter(os.Stdout)
-//	N, S   int
-//	nums   []int
-//	result int
-//)
-//
-//func toInt(n *int, buf []byte) {
-//	for _, v := range buf {
-//		*n = (*n)*10 + int(v-'0')
-//	}
-//	return
-//}
-//
-//func input() {
-//	var input string
-//	if sc.Scan() {
-//		input = sc.Text()
-//	}
-//	parts := strings.Split(input, " ")
-//	N, _ = strconv.Atoi(parts[0])
-//	S, _ = strconv.Atoi(parts[1])
-//
-//	if sc.Scan() {
-//		input = sc.Text()
-//	}
-//	parts = strings.Split(input, " ")
-//
-//	for _, part := range parts {
-//		num, _ := strconv.Atoi(part)
-//		nums = append(nums, num)
-//	}
-//
-//	sort.Ints(nums)
-//}
-//
-//func main() {
-//	input()
-//
-//	backTracking(0, 0)
-//
-//	if S == 0 {
-//		result -= 1
-//	}
-//	fmt.Fprintln(wr, result)
-//	wr.Flush()
-//}
-//
-//func backTracking(x int, total int) {
-//	if x == N && total == S {
-//		result += 1
-//		return
-//	}
-//	if x >= N {
-//		return
-//	}
-//	if total > S && nums[x] > 0 {
-//		return
-//	}
-//
-//	backTracking(x+1, total)
-//	backTracking(x+1, total+nums[x])
-//}
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
+
+var (
+	sc     = bufio.NewScanner(os.Stdin)
+	wr     = bufio.NewWriter(os.Stdout)
+	N, S   int
+	nums   []int
+	result int
+)
+
+func input() {
+	sc.Split(bufio.ScanWords)
+	N, S = nextInt(), nextInt()
+
+	nums = make([]int, N)
+	for i := 0; i < N; i++ {
+		nums[i] = nextInt()
+	}
+
+	sort.Ints(nums)
+}
+
+func main() {
+	input()
+	defer wr.Flush()
+
+	backTracking(0, 0)
+
+	if S == 0 {
+		result -= 1
+	}
+	fmt.Fprintln(wr, result)
+	wr.Flush()
+}
+
+func backTracking(x int, total int) {
+	if x == N && total == S {
+		result += 1
+		return
+	}
+	if x >= N {
+		return
+	}
+	if total > S && nums[x] > 0 {
+		return
+	}
+
+	backTracking(x+1, total)
+	backTracking(x+1, total+nums[x])
+}
+
+func nextInt() int {
+	sc.Scan()
+	text := sc.Text()
+	v, _ := strconv.Atoi(text)
+	return v
+}
+
+func nextInt64() int64 {
+	sc.Scan()
+	text := sc.Text()
+	v, _ := strconv.ParseInt(text, 10, 64)
+	return v
+}
