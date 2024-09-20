ee9267bbf3f20b73d88e1244950f039faaff9d09
Sang Hoo Park <sksk8922@gmail.com>
2024-09-20 23:56:48 +0900

    feat: 1520 dfs 구현

--- 1520/1520.py
+++ 1520/1520.py
@@ -1,15 +1,38 @@
 import sys
 
-f = open('../input.txt', 'r')
+f = open('input.txt', 'r')
 input = f.readline
 
+MAX = 10000
 # input = sys.stdin.readline
 
 N,M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[0 for _ in range(M)] for _ in range(N)]
+board = [[MAX for _ in range(M+2)]]+[[MAX] + list(map(int, input().split())) + [MAX] for _ in range(N)]+[[MAX for _ in range(M+2)]]
+visited = [[-1 for _ in range(M+2)] for _ in range(N+2)]
 
 delta = [(-1,0),(1,0),(0,-1),(0,1)]
 
-for dx,dy in delta:
-    print(dx,dy)
+# visited 체크
+# 더 낮은 숫자인지 확인
+# 더 낮은 숫자가 아닌 경우 return 0
+# 더 낮은 숫자인 경우 visited[y][x] += dfx(ny,nx)
+def dfs(y,x):
+    if visited[y][x] != -1:
+        return visited[y][x]
+    if y == N-1 & x == M-1:
+        return 1
+    visited[y][x] = 0
+
+    for dy,dx in delta:
+        ny,nx = y+dy,x+dx
+        if board[ny][nx] >= board[y][x]:
+            continue
+        visited[ny][nx] += dfs(ny,nx)
+
+dfs(1,1)
+
+print(visited[1][1])
+
+
+
+
