5fdb65bd311593ffbd7b46daba1136603a8d0568
Sang Hoo Park <sksk8922@gmail.com>
2024-09-21 00:05:28 +0900

    fix: 1520 오류 수정

--- 1520/1520.py
+++ 1520/1520.py
@@ -1,10 +1,13 @@
 import sys
 
-f = open('input.txt', 'r')
-input = f.readline
+sys.setrecursionlimit(10000)
 
-MAX = 10000
-# input = sys.stdin.readline
+# f = open('input.txt', 'r')
+# input = f.readline
+input = sys.stdin.readline
+
+MAX = 10001
+
 
 N,M = map(int, input().split())
 board = [[MAX for _ in range(M+2)]]+[[MAX] + list(map(int, input().split())) + [MAX] for _ in range(N)]+[[MAX for _ in range(M+2)]]
@@ -17,22 +20,23 @@ delta = [(-1,0),(1,0),(0,-1),(0,1)]
 # 더 낮은 숫자가 아닌 경우 return 0
 # 더 낮은 숫자인 경우 visited[y][x] += dfx(ny,nx)
 def dfs(y,x):
+    # 이미 방문한 경우 처리
     if visited[y][x] != -1:
         return visited[y][x]
-    if y == N-1 & x == M-1:
+    # 종점에 도착한 경우 처리
+    if y == N and x == M:
         return 1
+    # 처음 도착한 경우 초기화
     visited[y][x] = 0
 
     for dy,dx in delta:
         ny,nx = y+dy,x+dx
         if board[ny][nx] >= board[y][x]:
             continue
-        visited[ny][nx] += dfs(ny,nx)
+        visited[y][x] += dfs(ny,nx)
+
+    return visited[y][x]
 
 dfs(1,1)
 
 print(visited[1][1])
-
-
-
-
