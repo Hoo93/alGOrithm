268c40b75b68cc2c14191fd67e009f23b1df3949
Sang Hoo Park <sksk8922@gmail.com>
2024-10-15 23:51:15 +0900

    feat: 2357 시그먼트 트리 구현 생성 코드 구현

--- 2357/2357.py
+++ 2357/2357.py
@@ -7,9 +7,32 @@ input = file.readline
 
 # input = sys.stdin.readline
 
-N,M = map(int, input().split())
+INF = 1e9
 
-nums = [ int(input()) for _ in range(N) ]
+N, M = map(int, input().split())
+
+nums = [int(input()) for _ in range(N)]
+
+# [min, max]
+segment_tree = [[INF, 0] for _ in range(4*N)]
+
+
+def init(node, start, end):
+    if start == end:
+        segment_tree[node] = [nums[start], nums[start]]
+    else:
+        mid = (start + end) // 2
+
+        init(2 * node + 1, start, mid)
+        init(2 * node + 2, mid + 1, end)
+
+        segment_tree[node][0] = min(segment_tree[2 * node + 1][0], segment_tree[2 * node + 2][0])
+        segment_tree[node][1] = max(segment_tree[2 * node + 1][1], segment_tree[2 * node + 2][1])
+
+
+init(0, 0, N - 1)
+
+print(segment_tree)
 
 for _ in range(M):
     mn, mx = map(int, input().split())
