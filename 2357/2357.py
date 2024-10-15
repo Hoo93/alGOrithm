c08d068271945629d2287fa9a632c5ef9471d16b
Sang Hoo Park <sksk8922@gmail.com>
2024-10-15 23:59:59 +0900

    feat:2357 풀이

--- 2357/2357.py
+++ 2357/2357.py
@@ -14,7 +14,7 @@ N, M = map(int, input().split())
 nums = [int(input()) for _ in range(N)]
 
 # [min, max]
-segment_tree = [[INF, 0] for _ in range(4*N)]
+segment_tree = [[INF, 0] for _ in range(4 * N)]
 
 
 def init(node, start, end):
@@ -30,9 +30,24 @@ def init(node, start, end):
         segment_tree[node][1] = max(segment_tree[2 * node + 1][1], segment_tree[2 * node + 2][1])
 
 
-init(0, 0, N - 1)
+def solve(node, start, end, left, right):
+    if left > end or right < start:
+        return [INF, 0]
+    # 이 부분을 실수함
+    elif start >= left and end <= right:
+        # elif start < left and right < end: 기존 코드
+        return segment_tree[node]
+    else:
+        mid = (start + end) // 2
 
-print(segment_tree)
+        mn1, mx1 = solve(2 * node + 1, start, mid, left, right)
+        mn2, mx2 = solve(2 * node + 2, mid + 1, end, left, right)
+        return [min(mn1, mn2), max(mx1, mx2)]
+
+
+init(0, 0, N - 1)
 
 for _ in range(M):
     mn, mx = map(int, input().split())
+    result = solve(0, 0, N - 1, mn - 1, mx - 1)
+    print(result[0], result[1])
